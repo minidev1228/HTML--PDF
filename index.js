84feac0b452e22c88d3426432df3fcfc41042401
minidev1228 <mightybluedev@gmail.com>
2024-12-03 13:59:04 +0900

    All done

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const flags = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "
 async function generatePDFfromHTML(htmlContent, outputPath) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
   await page.setContent(htmlContent);
   await page.pdf({ path: outputPath, format: 'A4',printBackground: true });
   await browser.close();
 }
 
-async function getFirstLineOfThreeItems(arr) {
+async function getFirstLineOfItems(arr) {
 
-    let num = 0;
+    let num = 0 , end = 0;
 
-    if(arr.length<4) num = 40;
-    else num = 25;
+    if(arr.length<4) num = 35, end = 3;
+    else num = 22, end = 4;
 
     let content = `<div style="width: 100%;">
     <div style="width: 100%;display: flex;">`;
@@ -25,14 +26,44 @@ async function getFirstLineOfThreeItems(arr) {
         <div style="margin-left: 23px; height: 30px;width: 30px;background-color: black;color: white;display: flex;justify-content: center;align-items: center;border-radius: 50px;"><span>${flags[0]}</span></div>
         <p>${label}</p>
     </div>`
-    for(let i = 1;i<arr.length;i++){
+    for(let i = 1;i<end;i++){
         label = `${arr[i].state}-${arr[i].capital}-${arr[i].city}-${arr[i].currency}-${arr[i].currency_name}`
         content = `${content}<div style="border-top: 1px solid black;width: ${num}%;margin-top: 10px;">&nbsp;</div><div style="width: 100px;font-size: 12px;">
             <div style="margin-left: 23px; height: 30px;width: 30px;background-color: black;color: white;display: flex;justify-content: center;align-items: center;border-radius: 50px;"><span>${flags[i]}</span></div>
             <p>${label}</p>
         </div>`
     }
-    content=`${content}</div></div>`;
+    
+    if(arr.length > 4){
+        while(arr.length - end > 4) {
+            content = `${content}<div style="border-top: 1px solid black;width: 10px;margin-top: 10px;">&nbsp;</div></div></div><div style="width: 100%;">
+            <div style="width: 100%;display: flex;">`
+            for(let i = end; i < end + 4; i++){
+                label = `${arr[i].state}-${arr[i].capital}-${arr[i].city}-${arr[i].currency}-${arr[i].currency_name}`
+                content = `${content}<div style="border-top: 1px solid black;width: 22%;margin-top: 10px;">&nbsp;</div><div style="width: 100px;font-size: 12px;">
+                    <div style="margin-left: 23px; height: 30px;width: 30px;background-color: black;color: white;display: flex;justify-content: center;align-items: center;border-radius: 50px;"><span>${flags[i]}</span></div>
+                    <p>${label}</p>
+                </div>`
+            }
+            end += 4;
+        }
+        if(arr.length > end){
+            content = `${content}<div style="border-top: 1px solid black;width: 10px;margin-top: 10px;">&nbsp;</div></div></div><div style="width: 100%;">
+            <div style="width: 100%;display: flex;">`;
+            for(let i = end; i < arr.length; i++){
+                label = `${arr[i].state}-${arr[i].capital}-${arr[i].city}-${arr[i].currency}-${arr[i].currency_name}`
+                content = `${content}<div style="border-top: 1px solid black;width: 22%;margin-top: 10px;">&nbsp;</div><div style="width: 100px;font-size: 12px;">
+                    <div style="margin-left: 23px; height: 30px;width: 30px;background-color: black;color: white;display: flex;justify-content: center;align-items: center;border-radius: 50px;"><span>${flags[i]}</span></div>
+                    <p>${label}</p>
+                </div>`
+            }
+            content=`${content}</div></div>`;
+        } else {
+            content=`${content}</div></div>`;
+        }    
+    } else {
+        content=`${content}</div></div>`;
+    }
 
     return content;
 }
@@ -74,28 +105,33 @@ var htmlContent = `
     </div>
 `
 
-infos.forEach(async(info, id)=>{
-    // console.log(id,"===>",info);
-    htmlContent = `${htmlContent}
-        <div style="width: 95%;padding-left: 2%;">
-        <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 20px;">
-            <span>${info.date}</span>
-            <span>Day${id+1}</span>
-        </div>
-    `
-    let tripdata = info.tripData;
-    let newarr = [];
-    for(let i = 0; i<tripdata.length; i++){
-        if(i>3) break;
-        newarr.push(tripdata[i]);
+const run = async() =>{
+    for(let i = 0; i<infos.length; i++){
+
+        let info = infos[i];
+
+        htmlContent = `${htmlContent}
+            <div style="width: 95%;padding-left: 2%;margin-top:30px;">
+            <div style="width: 100%;display: flex;justify-content: space-between;margin-bottom: 20px;">
+                <span>${info.date}</span>
+                <span>Day${i+1}</span>
+            </div>
+        `
+        let tripdata = info.tripData;
+    
+        let content = await getFirstLineOfItems(tripdata);
+    
+        htmlContent = `${htmlContent}${content}</div>`;
+    
     }
-    let content = await getFirstLineOfThreeItems(newarr);
-    console.log(content);
-    htmlContent = `${htmlContent}${content}</div>`;
-})
+    
+    htmlContent = `${htmlContent}</div>`;
+    
+    // console.log(htmlContent);
+    
+    generatePDFfromHTML(htmlContent, 'custom.pdf')
+      .then(() => console.log('PDF generated successfully'))
+      .catch(err => console.error('Error generating PDF:', err));
+}
 
-htmlContent = `${htmlContent}</div>`;
-
-generatePDFfromHTML(htmlContent, 'custom.pdf')
-  .then(() => console.log('PDF generated successfully'))
-  .catch(err => console.error('Error generating PDF:', err));
+run();
